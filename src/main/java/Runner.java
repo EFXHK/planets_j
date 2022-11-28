c3c22bef41a2be7f043ec162aaa642359e0a6cd7
esau fong <esaufong@gmail.com>
2022-11-28 22:38:16 +0000

    planet vulcan java

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args) {
+        Planet vulcan = new Planet("Vulcan", 908973);
+
+        String name = vulcan.getName();
+        int size = vulcan.getSize();
+        System.out.println(name + size);
+        System.out.println(explode);
+    }
+}
